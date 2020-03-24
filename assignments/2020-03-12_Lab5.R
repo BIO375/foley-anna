fcc35f396da4db18efe3737454476fba8ad5cc0c
a-foley <annafoley_2020@depauw.edu>
2020-03-24 19:02:32 -0400

    update

--- assignments/2020-03-12_Lab5.R
+++ assignments/2020-03-12_Lab5.R
@@ -1,9 +1,161 @@
 ##Lab5####
+# Clean up the working environment
+rm(list = ls())
+##Load proper packages##
 
-library("tidyverse")
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(boot)){install.packages("boot")}
+if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(summarytools)){install.packages("summarytools")}
+if(!require(tidyverse)){install.packages("tidyverse")}
 
+# Check for updates
 tidyverse_update()
 
-#Question 1####
+#Problem 1####
 
-EarthAngleData <- read_csv("Data Sets Anna/EarthAngleData.csv")
+earthangle <- read_csv("Data Sets Anna/EarthAngleData.csv")
+
+###1.1 
+
+#The hypothesis is two sided, because we are seeing if the angle is different than the angle given by the paris observatory in 
+#1738. So it can either be greater or smaller for the alternative hypothesis 
+
+#therefore: a) angle is different from angles measures (u0 does not equal u previous)
+
+###1.2 
+
+#The null hypothesis is that the 1738 measurement is the same as the angles from the past 
+#(u0 = uprevious)
+
+##1.3 One sample T test 
+
+#two sided
+
+# Identify your response variable using the form dataset$variable_name
+y<-earthangle$Obliquity
+
+#Calculate Summary Statistics 
+sample_mean <-mean(y)
+sample_sd <- sd(y)
+sample_n <- as.numeric(length(y))
+df <- sample_n -1
+alpha=0.05
+
+#Null mean 
+null_mean <- 23.4722
+
+#calculate t sample 
+t_sample <- (sample_mean - null_mean)/(sample_sd/sqrt(sample_n))
+
+# For a two-sided test, the exact probability of obtaining t equal to t_sample or more extreme is calculated
+# as:
+two_tailed <- 2*(1-pt(abs(t_sample), df))
+
+# Other way to calculate Two-sided
+t.test(earthangle$Obliquity, 
+       alternative = "two.sided", mu = 23.4722, conf.level = 0.95)
+
+##We found the obliquity measured in Paris in 1738 was statistically different when compared with 
+#earlier measurements (one-sample t-test: t = 3.03; df = 4; P=0.04)
+
+##Problem 2####
+
+heartattack <- read_csv("datasets/demos/HeartAttack_short.csv")
+
+
+##2.1 
+
+#two-sided because we are looking for just a difference (not greater or less than)
+#Group 1 is Heart attack 
+#Group 2 Control 
+
+#Statistical alternative hypothesis 
+#u1 - u2 ≠ 0 
+
+#Statistical Null hypothesis 
+#u1 - u2 = 0
+
+##2.2 
+
+#df = n1 + n2 -2 
+# = 28 + 30 -2
+# degrees of freedom is 56
+
+
+##Calculate summary statistics for each group 
+
+ heart_summary <- heartattack %>%
+  group_by(`group`) %>%
+  summarise(n_heart = n(),
+            mean_heart = mean(cholest ),
+            median_heart = median(cholest ),
+            sd_heart = sd(cholest ),
+            IQR_heart = IQR(cholest ),
+            var_heart = var(cholest ),
+            se_heart = sd(cholest )/sqrt(n()))
+
+##2.3 (2.4)
+
+##Check normality assumption 
+ # Look at histograms, box plots, q-q plots
+ 
+ ggplot(heartattack) +
+   geom_histogram(aes(cholest), binwidth = 25)+
+   facet_wrap(~group)
+ 
+ ggplot(data = heartattack, mapping = aes(group = group, x = group, y = cholest)) + 
+  geom_boxplot()+
+   stat_summary(aes(x = group, y = cholest), 
+                fun.y=mean, 
+                colour="green", 
+                fill = "green",
+                geom="point", 
+                shape=21, 
+                size=3)
+ 
+ ggplot(heartattack)+
+   geom_qq(aes(sample = cholest, color = group))
+ 
+##Try mutating the data 
+heartattack <- heartattack %>%
+   mutate(log_cholest = log(cholest))
+
+##Check normality again
+ggplot(heartattack) +
+  geom_histogram(aes(log_cholest), binwidth = 0.1)+
+  facet_wrap(~group)
+
+ggplot(data = heartattack, mapping = aes(group = group, x = group, y = log_cholest)) + 
+  geom_boxplot()+
+  stat_summary(aes(x = group, y = log_cholest), 
+               fun.y=mean, 
+               colour="green", 
+               fill = "green",
+               geom="point", 
+               shape=21, 
+               size=3)
+
+ggplot(heartattack)+
+  geom_qq(aes(sample = log_cholest, color = group))
+
+# In both histograms, it seems to be a generally normal distribution, with a larger range for group 1 compared
+#to group 2. There also shows a tail in the lower end for group 1. As for the boxplots, it shows an outlier&longer whisker and right skew for 
+#group 2, with the mean being larger than the median. For group1, the whiskers are relatively the same length but the mean is more centered
+#in the middle of the IQR than the median. But, similar to the ward snail data, the samples sizes of 28 and 30 are large so even with the slight
+#deviations from normality, a parametric test is still okay 
+
+
+# Calculate the ratio between the standard deviations as a loose test of homoscedasticity
+ ratio <-(max(heart_summary$sd_heart))/(min(heart_summary$sd_heart))
+
+##The ratio is 2.20, which means it is less than or equal to 3, so the variances are homegenous enough for the parametric test
+ 
+# With both of these assumptions that are not violated, then we can assume the two sample t test is reliable 
+ 
+##2.5 perform 2 sample t test 
+ 
+
+
+
