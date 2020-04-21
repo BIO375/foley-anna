6b4271b5235628207aaddf16203a2f58b80b6e5c
a-foley <annafoley_2020@depauw.edu>
2020-04-21 11:32:16 -0400

    almost done lab 7

--- assignments/2020-04-20_Lab8.R
+++ assignments/2020-04-20_Lab8.R
@@ -89,24 +89,146 @@ anova(model23)
 # Summary of the model results
 summary(model23)
 
-
-### Multiple Comparisons ####
-
-# Earlier you installed and loaded the package multcomp.  To do planned comparisons
-# we will use a function in multcomp called glht, short for general linear
-# hypotheses.
-# linfct specifies the linear hypotheses to be tested, I find the easiest way
-# is to specify by name
+##We found that the conemass varied significantly with habitat (F=50.085, df=2,13, p=7.87e-7)
 
 # Planned comparisons
 
-planned <- glht(model01, linfct = 
-                  mcp(parasite = c("Metschnikowia - control = 0",
-                                   "Pansporella - control = 0",
-                                   "Pasteuria - control = 0")))
+planned <- glht(model23, linfct = 
+                  mcp(habitat = c("island.absent-island.present = 0"
+                      )))
 confint(planned)
 summary(planned)
 
-###15-26####
+##Using planned comparisons, the island absent showed significantly higher pinecone mass with island absent 
+#compared to island present (t=8.596, p = 1.01e-6)
+
+###############15-26####
+
+malaria <- read_csv("datasets/abd/chapter15/chap15q26MalariaFungusVenom.csv", col_types = cols(
+  treatmentGroup = col_factor()))
+
+
+##15-26 Step 1####
+ggplot(malaria) +
+  geom_histogram(aes(logSporozoiteNumbers), binwidth = 1)+
+  facet_wrap(~treatmentGroup)
+
+ggplot(malaria) +
+  geom_boxplot(aes(x = treatmentGroup, y = logSporozoiteNumbers))+
+  stat_summary(aes(x = treatmentGroup, y = logSporozoiteNumbers), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "blue",
+               geom="point", 
+               shape=21, 
+               size=3)
+
+ggplot(malaria)+
+  geom_qq(aes(sample = logSporozoiteNumbers, color = treatmentGroup))
+
+
+##the histograms do not really give you much aboutt the normality of the data, but you do see an 
+#outlier in the scorpine group.The boxplots look pretty symmetrical for control and WT.Control and WT 
+#have median and mean centered in the IQR with almost equal whiskers. 
+#But the scorpine group has outliers on both sides and a very different mean compared to median 
+#and the median not centered in the IQR. The qq plot looks linear for control and WT, but the scorpine
+#has the very large outlier. I believe that this makes the data non-parametric so we must perform a 
+#Non-parametric Kruskal-Wallis test
+
+#15-26 Step 2####
+
+model26 <- lm(logSporozoiteNumbers~treatmentGroup, data = malaria)
+
+#15-26 Step 3####
+
+autoplot(model26)
+
+##The plot looks a bit triangular, so I do believe the decision to do the non-parametric test was correct.
+
+### 15-26 Step 4 ####
+
+kruskal.test(logSporozoiteNumbers ~ treatmentGroup, data = malaria)
+
+##The KW Rank sum test shows a statistical difference between the groups
+#(chi-squared = 22.874, df=2, p=1.079e-5)
+
 
 ###15-30 & 31####
+
+crabs <- read_csv("datasets/abd/chapter15/chap15q30FiddlerCrabFans.csv", col_types = cols(
+  crabType = col_factor()))
+
+crabs <- na.omit(crabs)
+
+##15-30 Step 1####
+
+ggplot(crabs) +
+  geom_histogram(aes(bodyTemperature), binwidth = 0.4)+
+  facet_wrap(~crabType)
+
+ggplot(crabs, aes(x = crabType, y = bodyTemperature))+
+  geom_boxplot() +
+  theme_bw() +
+  coord_flip()+
+  stat_summary(aes(x = crabType, y = bodyTemperature), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "blue",
+               geom="point", 
+               shape=21, 
+               size=3)
+  
+ggplot(crabs)+
+  geom_qq(aes(sample = bodyTemperature, color = crabType))
+
+##Check for equal variance 
+
+summ_crabs <- crabs %>%
+  group_by(crabType) %>% 
+  summarise(mean_temp = mean(bodyTemperature),
+            sd_temp = sd(bodyTemperature),
+            n_temp = n())
+
+
+ratio <-(max(summ_crabs$sd_temp))/(min(summ_crabs$sd_temp))
+
+#The histograms do not really demosntrate a large skew or any abnormalities. The boxplot for each group
+#has pretty similar means and medians, with the medians being pretty centered in the IQR. The whiskers for 
+#female are similar length. The bottom whisker is slightly longer for intact male and minor male removed.
+#THe only problem with these plots is that there are outliers in 3 of the 4 groups. However, the n is pretty large
+#so I feel it may still be appropriate to perform the parametric test. The ratio of sd is 1.18, so there are 
+#equal variances.
+
+
+#15-30 Step 2####
+
+model30 <- lm(bodyTemperature~crabType, data = crabs)
+
+#15-30 Step 3####
+
+autoplot(model30)
+
+##The plot looks pretty rectangular and not so much like a square, so I believe although there are a 
+#couple of outliers, it is still okay to do the ANOVA test 
+
+### 15-30 Step 4 ####
+
+anova(model30)
+
+# Summary of the model results
+summary(model30)
+
+##We found that the conemass varied significantly with habitat (F=50.085, df=2,13, p=7.87e-7)
+
+# Planned comparisons
+
+planned <- glht(model23, linfct = 
+                  mcp(habitat = c("island.absent-island.present = 0"
+                  )))
+confint(planned)
+summary(planned)
+
+##Using planned comparisons, the island absent showed significantly higher pinecone mass with island absent 
+#compared to island present (t=8.596, p = 1.01e-6)
+
+
