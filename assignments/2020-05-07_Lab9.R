842844f66a4ead4472a83f681d9a2dd4e0470f35
a-foley <annafoley_2020@depauw.edu>
2020-05-09 17:59:26 -0400

    done lab 9

--- assignments/2020-05-07_Lab9.R
+++ assignments/2020-05-07_Lab9.R
@@ -102,6 +102,85 @@ earwigCor
 #1.random sample
 #2.relationship between 2 ranks is linear 
 
+
+
+
 ##Chapter 17-6####
 
 zoo <- read_csv("datasets/abd/chapter17/chap17q06ZooMortality.csv")
+
+##6 a) draw scatterplot
+ggplot(data = zoo, aes(x = logHomerange, y = mortality)) +
+  geom_point(colour = "purple") 
+
+##elliptical shape, looks like linear relationship 
+
+##fit the model 
+
+model01 <- lm(mortality ~ logHomerange, data = zoo)
+
+autoplot(model01, smooth.colour = NA)
+
+ggplot(data = zoo)+
+  geom_point(aes(x = logHomerange, y = resid(model01)))  
+
+
+summary(model01)
+
+##The mortality rate significantly increases with home range size in the wild
+#(Linear regression: Mortality = 15.370 + 10.264(logHomerange);
+# df = 1, 18, F=14.52, P<0.00128), and log homerange size explained more than 40%
+# of the variability in nose blackness (R2 = 0.4465).
+q
+##add linear model to graph 
+
+ggplot(data = zoo, aes(x = logHomerange, y = mortality)) +
+  geom_point() +
+  geom_smooth(method = "lm", level=0.95) +
+  theme_bw()+
+  labs( x = "Home range (log10)", y = "Mortality")
+
+##Remove the outlier from the data 
+
+zoo_2 <- zoo[-c(20),]
+
+##6 a) draw scatterplot
+ggplot(data = zoo_2, aes(x = logHomerange, y = mortality)) +
+  geom_point(colour = "blue") 
+
+##elliptical shape, looks like linear relationship 
+
+##fit the model 
+
+model02 <- lm(mortality ~ logHomerange, data = zoo_2)
+
+autoplot(model02, smooth.colour = NA)
+
+ggplot(data = zoo_2)+
+  geom_point(aes(x = logHomerange, y = resid(model02)))  
+
+
+summary(model02)
+
+##The mortality rate significantly increases with home range size in the wild
+#(Linear regression: Mortality = 17.510 + 6.600(logHomerange);
+# df = 1, 17, F=4.611, P<0.0465), and log homerange size explained more than 20%
+# of the variability in nose blackness (R2 = 0.2134).
+
+
+##add linear model to graph 
+
+ggplot(data = zoo_2, aes(x = logHomerange, y = mortality)) +
+  geom_point() +
+  geom_smooth(method = "lm", level=0.95) +
+  theme_bw()+
+  labs( x = "Home range (log10)", y = "Mortality")
+
+##Changed the slope by almost half!
+
+
+
+
+
+
+
