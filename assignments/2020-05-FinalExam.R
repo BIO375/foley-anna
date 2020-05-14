5eb1cf8582204a3b0dff91d63050bfe68b611c93
a-foley <annafoley_2020@depauw.edu>
2020-05-14 14:53:22 -0400

    done final exam :)

--- assignments/2020-05-FinalExam.R
+++ assignments/2020-05-FinalExam.R
@@ -17,16 +17,16 @@ insulation <- read_csv("datasets/demos/insulation.csv")
 
 #Identify Variables
 
-#Response variable: change in body temperature/min (continuous)
-#Predictor Variable: "leaness": adjusted skin fold tickness (coontinous)
+#Response variable: "heat_loss": change in body temperature/min (continuous)
+#Predictor Variable: "leanness": adjusted skin fold thickness index (continuous)
 
 #b####
 ##Statistical null
 
-#slope of the fit line =0, B = 0
+#slope of linear model = 0, b1 = 0
 
-##c####
-##Default test for two continous variables and testing the causal relationship between them is linear regression
+##c & d ####
+##Default test for two continuous variables and testing the causal relationship between them is linear regression
 ##Assumption of linear regression: 
 
 #i) Relationship is linear? 
@@ -47,22 +47,48 @@ autoplot(model01, smooth.colour = NA)
 ggplot(data = insulation)+
   geom_point(aes(x = leanness, y = resid(model01)))
 
-##Although it does not look perfect because of the pretty small sample size, it does not look fan shaped, 
-#which means that the data meet the assumptions for a linear regression
+##Although it does not look perfect because of the pretty small sample size, the residual plot does not look fan shaped
+#even with the outliers and the normal Q-Q plot does not look terribly not linear which means that the data meet the assumptions for 
+#a linear regression. Doing transformations such as square root or log did not improve the data significantly, 
+#and the residual plot for log might even look a bit fan-shaped, so I do not believe a transformation is warranted. 
 
-#d####
+##sqrt
+insulation <- insulation %>%
+  mutate(sqrt_heat = sqrt(heat_loss))
 
-#The appropriate staistical test is linear regression, because we are not just looking for an association between
-#the two varibales, but a causal relationship between leanness and heat loss 
+model02<-lm(sqrt_heat ~ leanness, data = insulation)
+
+autoplot(model02, smooth.colour = NA)
+ggplot(data = insulation)+
+  geom_point(aes(x = leanness, y= resid(model02)))
+
+##log
+insulation <- insulation %>%
+  mutate(log10_heat = log10(heat_loss))
+
+model03<-lm(log10_heat ~ leanness, data = insulation)
+
+autoplot(model03, smooth.colour = NA)
+
+ggplot(data = insulation)+
+  geom_point(aes(x = leanness, y= resid(model03)))
+
+
+#The appropriate statistical test is linear regression, because we are looking to test the
+#causal relationship between leanness and heat loss and how much variation in heat loss is 
+#caused but leanness
 
 ##e####
 
 summary(model01)
 
 ##Boys with higher leanness scores have significantly more heat loss 
-#(Linear Regression: heat_loss = -0.0269 + 0.0190(leanness);
+#(Linear Regression: heat_loss = -0.02691 + 0.01897(leanness);
 #df = 1,10, F=68.73, p=8.61e-06) and leanness explained more than 85% of the variability in
 #heat loss (R2 = 0.873)
+
+
+##add regression line to plot 
 
 ggplot(data = insulation, aes(x = leanness, y = heat_loss)) +
   geom_point() +
